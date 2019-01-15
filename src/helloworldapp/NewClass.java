88005e591a11596de9dc5a172bc3b0635d6c4674
declaneparker <declaneparker@labpc222.computing.dundee.ac.uk>
2019-01-15 12:44:09 +0000

    Initial Commit

---
+++ src/helloworldapp/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldapp;
+
+/**
+ *
+ * @author declaneparker
+ */
+class NewClass {
+    System.out.println("Hello, World!");
+}
